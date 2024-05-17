8f55f11db1e4b0f726d493998f573b1e32f33cf7
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-17 07:40:04 -0600

    added heroku env var

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { Socket, io } from 'socket.io-client';
 import { ClientToServerSocketEventTypes, ServerToClientSocketEventTypes } from './server/src/socket';
 
-const URL = 'http://localhost:8080';
+const URL = import.meta.env.VITE_SOCKET_URL as string ?? 'http://localhost:8080';
 
 export const socket: Socket<ServerToClientSocketEventTypes, ClientToServerSocketEventTypes> = io(URL);
