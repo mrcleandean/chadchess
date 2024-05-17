88965b37f510d0edef3807e6a4066b3bcb89c03c
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-17 07:56:10 -0600

    fixed vercel build errors

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('Socket.io server for ChadChess');
 })
 
