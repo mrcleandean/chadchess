f261788812b66099fbb811ef17bb823046bc20fb
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-20 07:02:55 -0600

    removed unused asset import

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -6,8 +6,7 @@ import rambo from './characters/rambo.png';
 import squidward from './characters/squidward.png';
 import stewie from './characters/stewie.png';
 import tate from './characters/tate.png';
-import bg from './bg.png';
 import chadchessbg from './chadchessbg.jpg';
 import logo from './logo.png';
 
-export { chest, gigachad, liam, mega, rambo, squidward, stewie, tate, bg, chadchessbg, logo };
+export { chest, gigachad, liam, mega, rambo, squidward, stewie, tate, chadchessbg, logo };
