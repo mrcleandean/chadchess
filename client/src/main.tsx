576dc53c348c880f4cf952709d5480af9b3aa352
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-20 07:11:31 -0600

    removed unnecessary fragment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,7 @@ import Header from './components/Header.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <>
-      <Header />
-      <Providers />
-    </>
+    <Header />
+    <Providers />
   </React.StrictMode>,
 )
