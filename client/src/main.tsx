95afab2e45564007ca7e1d604bdc51ff510df7ea
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-22 13:04:50 -0600

    added vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Providers from './components/Providers.tsx'
 import Header from './components/Header.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics />
     <Header />
     <Providers />
   </React.StrictMode>,
